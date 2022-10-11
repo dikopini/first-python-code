ecf27deead9a86d03e19025da72ecceed4cf7a2c
Irfan Basyaruddin <irfanbasyar.ib@gmail.com>
2022-10-11 14:49:59 +0700

    pagination

--- run.py
+++ run.py
@@ -2,17 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 
 
+url = 'https://www.yachtworld.com/boats-for-sale/?makeModel=hunter&page='
+header = {
+    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+}
+
+def pagination():
+    res = requests.get(url, headers=header)
+    soup =BeautifulSoup(res.text, 'html.parser')
+    pages = soup.find('div', {'class':'search-page-nav'})
+    total_page = pages.find('a')
+    print(total_page)
+
+
 def content():
-    url = 'https://www.yachtworld.com/boats-for-sale/?makeModel=hunter&page='
-    res = requests.get(url)
+    res = requests.get(url, headers=header)
     soup = BeautifulSoup(res.text, 'html.parser')
     contents = soup.find_all('div', {'class': 'listing-card-information'})
 
+    boat_list = []
     for info in contents:
         name = info.find('h2').text
-        print(name)
+        price = info.find('span').text
+        location = info.find('div', {'class': 'listing-card-location'}).text
+        broker = info.find('div', {'class': 'listing-card-broker'}).text
 
+        datas = {
+        'name': name,
+        'price': price,
+        'location': location,
+        'broker': broker
+        }
+
+        boat_list.append(datas)
 
 
 if __name__ == '__main__':
-    content()
+    pagination()
